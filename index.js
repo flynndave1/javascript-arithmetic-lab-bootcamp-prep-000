92659dfbb5fddb5c30a997e3eab4c14834d095e9
david m flynn <flynndave1@msn.com>
2018-07-28 16:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ function add ( a , b ) {
 console.log(TT)
 
 function subtract ( a , b ) {
-  sum = a + b;
+  sum = a - b;
   return sum;
 }
  TT = subtract(54 , 78)
 console.log(TT)
 
 function multiply ( a , b ) {
-  sum = a + b;
+  sum = a * b;
   return sum;
 }
  TT = multiply(54 , 78)
 console.log(TT)
 
 function divide ( a , b ) {
-  sum = a + b;
+  sum = a / b;
   return sum;
 }
  TT = divide(54 , 78)
